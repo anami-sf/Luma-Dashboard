b2c8fd5410e90ba8d02c9501c546ff05424ebb34
Ana <anami823@gmail.com>
2019-03-10 19:14:44 -0700

    Orginized files into folders.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import './App.css';
-import { setCadance } from './actions';
+import { setCadance } from '../../actions';
 
 const mapStateToProps = (state) => {
   return{
